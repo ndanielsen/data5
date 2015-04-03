649f5a5a57d55fe6ad2ea0f04942a638103dc739
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-04-02 23:45:41 -0400

    DataViz All done except for part3

--- hw/hw4/hw4.py
+++ hw/hw4/hw4.py
@@ -42,28 +42,78 @@ class VizCar(object):
 		self.df = pd.read_table(self.input_file, sep='|')
 
 	def part1(self):
-
 		'''
 		Part 1
 		Produce a plot that compares the mean mpg for the different numbers of cylinders.
 		'''
 
-		self.part1_graphic = "Graphic"
+		self.df.groupby('cylinders').mpg.mean().plot(kind='bar')
+		plt.title("Comparing Mean MPG for Different Numbers of Cylinders")
+		plt.ylabel("MPG")
+		plt.savefig('cylinders_by_mean_mpg.png', dpi=50)
 
-		self.part1_answer = "Something"
+		self.part1_code = 	\
+		"""
+		self.df.groupby('cylinders').mpg.mean().plot(kind='bar')
+		plt.title("Comparing Mean MPG for Different Numbers of Cylinders")
+		plt.ylabel("MPG")
+		plt.savefig('cylinders_by_mean_mpg.png', dpi=50)
+		"""
+
+		self.part1_graphic =  'cylinders_by_mean_mpg.png' 
+
+		self.part1_answer = "It seems clear that the most efficent number of cylinders is four and declines steeply after that."
 		pass
 
 
 	def part2(self):
-
 		'''
 		Part 2
 		Use a scatter matrix to explore relationships between different numeric variables.
 		'''
-		self.part2_graphic = "Graphic"
 
-		self.part2_answer = "Something"
-		pass
+		self.dfnum = self.df._get_numeric_data()
+
+		pd.scatter_matrix(self.dfnum, alpha=0.2, diagonal='hist')
+		
+		plt.savefig('scatter_matrix.png', dpi=250)
+
+		self.part2_code = 	\
+		"""
+		self.dfnum = self.df._get_numeric_data()
+		pd.scatter_matrix(self.dfnum, alpha=0.2, diagonal='hist')
+		plt.savefig('scatter_matrix.png', dpi=250)
+
+		"""
+		self.part2_graphic = 'scatter_matrix.png'
+
+		self.part2_answer = "After looking at the entire scatter plot of numeric data, it seems that MPG is the most interesting variable to compare, so I'll make another scatterplot"
+		
+
+	def part2a(self):
+		'''
+		Part 2
+		Use a scatter matrix to explore relationships between different numeric variables.
+		'''
+
+		
+
+		self.dfnum.groupby('mpg').mean().hist(alpha=0.5, bins=15)
+		
+		plt.savefig('histo_matrix_mpg.png', dpi=250)
+
+		self.part2a_code = 	\
+		"""
+		self.dfnum.groupby('mpg').mean().hist(alpha=0.5, bins=15)
+		plt.savefig('histo_matrix_mpg.png', dpi=250)
+		"""
+		self.part2a_graphic = 'histo_matrix_mpg.png'
+
+		self.part2a_long_answer = "For acceleration, it seems clear that there is a sweet spot around 16.5 for best MPG \
+		For displacement, weight and horsepower it seems clear that as they increase, MPG goes down. \
+		For model year, it trends than over time the MPG gets better."
+		
+
 
 
 	def part3(self):
@@ -78,56 +128,81 @@ class VizCar(object):
 		create a new column that encodes whether a year is before or after 1975.)
 		'''
 
-		self.part3_graphic = "Graphic"
+		self.part3_code = 	\
+		"""
+		"""
 
-		self.part3_answer = "Something"
+		self.part3_graphic = "http://media.giphy.com/media/13HBDT4QSTpveU/giphy.gif"
+
+		self.part3_answer = "In progress"
 	pass
 
 	def answers(self):
 
 		print "#GA-DC DAT5 Homework Four Visualizing Auto Data"
 		print "######_With just enough illustrative code_"
-
-
+		
 		print "###Part 1"
 		print "\n\n"
 		print self.part1.__doc__
-		
-		print "\n"
-		print self.part1_graphic 
 		print "\n"
 
-		print "_" + self.part1_answer +"_"
+		print "![%s](%s)" % ("Image", self.part1_graphic)
+		print "\n"
+
+		print self.part1_code
+		print "\n"
+
+		print "_%s_" % self.part1_answer
 
 
 		print "###Part 2"
 		print "\n\n"
 		print self.part2.__doc__
-
-		print "\n"
-		print self.part2_graphic 
 		print "\n"
 
-		print "_" + self.part2_answer +"_"
+		print "![%s](%s)" % ("Image", self.part2_graphic)
+		print "\n"
+
+		print self.part2_code
+		print "\n"
+
+		print "_%s_" % self.part2_answer
+
+
+		print "####Taking a closer look at MPG and how other average variables relate to it"
+		print "\n"
+		print "![%s](%s)" % ("Image", self.part2a_graphic)
+		print "\n"
+
+		print self.part2a_code
+		print "\n"
+
+		print "_%s_" % self.part2a_long_answer
 
 
 		print "###Part 3"
 		print "\n\n"
 		print self.part3.__doc__
-
-		print "\n"
-		print self.part3_graphic
 		print "\n"
 
-		print "_" + self.part3_answer + "_"
+		print "![%s](%s)" % ("Image", self.part3_graphic)
+		print "\n"
+
+		print self.part3_code
+		print "\n"
+
+		print "_%s_" % self.part3_answer
 		pass
 
 
 
 	def main(self):
 
+		self.setUP()
 		self.part1()
 		self.part2()
+		self.part2a()
 		self.part3()
 		self.answers()
 
--- hw/hw4/readme.md
+++ hw/hw4/readme.md
@@ -11,10 +11,18 @@
 		
 
 
-Graphic
+![Image](cylinders_by_mean_mpg.png)
 
 
-_Something_
+
+		self.df.groupby('cylinders').mpg.mean().plot(kind='bar')
+		plt.title("Comparing Mean MPG for Different Numbers of Cylinders")
+		plt.ylabel("MPG")
+		plt.savefig('cylinders_by_mean_mpg.png', dpi=50)
+		
+
+
+_It seems clear that the most efficent number of cylinders is four and declines steeply after that._
 ###Part 2
 
 
@@ -25,10 +33,32 @@ _Something_
 		
 
 
-Graphic
+![Image](scatter_matrix.png)
 
 
-_Something_
+
+		self.dfnum = self.df._get_numeric_data()
+		pd.scatter_matrix(self.dfnum, alpha=0.2, diagonal='hist')
+		
+		plt.savefig('scatter_matrix.png', dpi=250)
+
+		
+
+
+_After looking at the entire scatter plot of numeric data, it seems that MPG is the most interesting variable to compare, so I'll make another scatterplot_
+####Taking a closer look at MPG and how other average variables relate to it
+
+
+![Image](histo_matrix_mpg.png)
+
+
+
+		self.dfnum.groupby('mpg').mean().hist(alpha=0.5, bins=15)
+		plt.savefig('histo_matrix_mpg.png', dpi=250)
+		
+
+
+_For acceleration, it seems clear that there is a sweet spot around 16.5 for best MPG 		For displacement, weight and horsepower it seems clear that as they increase, MPG goes down. 		For model year, it trends than over time the MPG gets better._
 ###Part 3
 
 
@@ -45,7 +75,11 @@ _Something_
 		
 
 
-Graphic
+![Image](http://media.giphy.com/media/13HBDT4QSTpveU/giphy.gif)
 
 
-_Something_
+
+		
+
+
+_In progress_
